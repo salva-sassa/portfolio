c416dc363102189685817ea27be1525dedffd1d2
SalvadorSassatelli <salvasassa3@gmail.com>
2025-03-12 16:30:06 -0300

    refactor: refactor UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import ThemeProvider from "./components/ThemeProvider";
 import ThemeToggle from "./components/ThemeToggle";
 import RotatingLogo from "./components/RotatingLogo";
 import Aurora from './components/Aurora';
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export const metadata: Metadata = {
   title: "Salvador Sassatelli - Portfolio",
@@ -36,7 +37,7 @@ export default function RootLayout({
           disableTransitionOnChange
           storageKey="portfolio-theme"
         >
-          <div className="absolute bottom-0 left-0 w-full h-[20%] opacity-65 z-[-1] rotate-180">
+          <div className="absolute bottom-0 left-0 w-full h-full opacity-65 z-[-1] rotate-180">
              {/* 
             <Aurora
               colorStops={["#1e1e1f", "#0A0E12", "#19212f"]}
@@ -45,6 +46,7 @@ export default function RootLayout({
               speed={0.5}
             />
             */}
+            <BackgroundBeams />
           </div>
         
           <div className="max-w-7xl w-full h-full md:h-[calc(80vh-2rem)] mb-20 md:mb-[12vh] grid grid-cols-1 sm:grid-cols-4 md:grid-cols-8 lg:grid-cols-7 gap-8">
